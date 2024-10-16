5dc7d71f527ab7c8a9d8b8f3985092b39a02d6d5
Wren <grahamgrasley@gmail.com>
2024-10-16 15:20:04 -0400

    add serving count

--- Backend/Models/Recipe.cs
+++ Backend/Models/Recipe.cs
@@ -9,6 +9,7 @@ public partial class Recipe
     public string? Name { get; set; }
 
     public string? UserId { get; set; }
+    public int Servings { get; set; }
 
     public virtual ICollection<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
 
